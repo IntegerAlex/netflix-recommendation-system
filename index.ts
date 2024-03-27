2624945f68cc0309203210a8aefbdf72282540b6
akshat <akshat@debian.localhost>
2024-03-27 17:24:23 +0530

    added requirement.txt

--- index.ts
+++ index.ts
@@ -86,8 +86,8 @@ app.get('/recommendation', (req, res) => {
 
 
 
-app.listen(3000, () => {    
-    console.log('Server is running on port 3000');
+app.listen(8080, () => {    
+    console.log('Server is running on port 8080');
 });
 
 
